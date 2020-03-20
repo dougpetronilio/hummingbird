72d0f70773f88eb9cf7d7649c1af17f616df16f0
douglas.petronilio <dougpetronilio@gmail.com>
2020-03-20 18:47:59 -0300

    Improve path to crawl

--- api/report/management/commands/update_report.py
+++ api/report/management/commands/update_report.py
@@ -25,7 +25,7 @@ def cron(*args, **options):
                                     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
 
         bucket_name = settings.AWS_STORAGE_BUCKET_NAME
-        url = "%s%s"%(settings.BINO_URL, 'crawl/ministerio_saude_brasil')
+        url = "%s%s"%(settings.BINO_URL, '/crawl/ministerio_saude_brasil')
         response = requests.post(url = url, data = {})
         file_name = json.loads(response.content.decode('utf-8'))['path']
 
